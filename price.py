eb382edfb2d429670d484affd91822b5ec58d839
root <root@monerobux.localdomain>
2019-08-14 23:41:04 +0000

    update before pull

--- price.py
+++ price.py
@@ -783,7 +783,7 @@ def xmrtall(bot, trigger):
 	# Finally, price in BTC, and volume in XMR
 	  thumbBTCxmr = thumbXMRkrw/thumbBTCkrw
 	  thumbXMRVol = float(xmrjson['data']['volume_1day'])
-	  stringtosend = "Bithumb last: {0:.6f} BTC on {1:.2f} XMR volume | ".format(thumbBTCxmr,thumbXMRVol)
+	  stringtosend += "Bithumb last: {0:.6f} BTC on {1:.2f} XMR volume | ".format(thumbBTCxmr,thumbXMRVol)
     except:
 	  bot.say("Error - bithumb korea is worst korea.")
 
